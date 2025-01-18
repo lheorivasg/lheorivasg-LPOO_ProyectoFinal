c4ddf56b330480d439cb16d945327880a30f1980
Kirig <Kirig@Kiri>
2025-01-18 14:00:05 -0600

    modificacion para realizar conexion BD 1-18-2025

--- src/java/modelo/Socio.java
+++ src/java/modelo/Socio.java
@@ -5,10 +5,88 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Equipo 5: Max Alvarez Alvarez, Hugo Rubio Romero y Leonardo Rivas Gutierrez
  */
 public class Socio {
+    private String idSocio;
+    private String nombre;
+    private LocalDate fechaNacimiento ;
+    private String telefono;
+    private String email;
+    private LocalDate fechaInscripcion;
+    private String membresia;
+    private String estado;
+
+    public String getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(String idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public void setFechaInscripcion(LocalDate fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public String getMembresia() {
+        return membresia;
+    }
+
+    public void setMembresia(String membresia) {
+        this.membresia = membresia;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+    
     
 }
